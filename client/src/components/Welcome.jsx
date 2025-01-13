1fe413d1356b146df0e54a70946878cf632bf322
obaidallah <obaidalqershi1@gmail.com>
2025-01-13 16:47:53 +0300

    add: the transaction providers

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-constant-condition */
 // import { AiFillPlayCircle } from "react-icons/ai"
+import React, {useContext} from 'react'
 import {SiEthereum} from 'react-icons/si';
 import {BsInfoCircle} from 'react-icons/bs';
 import {Loader} from './';
+import { TransactionContext } from '../context/TransactionContext';
+
+
 const commonStyles = "min-h-[70px] sm:px-2 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 const Input = ({placeholder, name, type, value, handleChange})=>(
   <input 
@@ -16,6 +20,8 @@ const Input = ({placeholder, name, type, value, handleChange})=>(
   />
 )
 const Welcome = () => {
+  const {value} = useContext(TransactionContext);
+  console.log(value)
   const connectWallet= ()=>{
 
   }
@@ -64,9 +70,9 @@ const Welcome = () => {
       <div className="flex justify-between flex-col w-full h-full">
         <div className="flex justify-between items-start">
           <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-            <SiEthereum fontsize={21} color="#fff" />
+            <SiEthereum fontSize={21} color="#fff" />
           </div>
-          <BsInfoCircle fontsize={17} color='#fff' />
+          <BsInfoCircle fontSize={17} color='#fff' />
         </div>
         <div>
           <p className='text-white font-light text-sm'>
